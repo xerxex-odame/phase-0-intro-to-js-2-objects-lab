b5a32fca646325f9361413624e1cabe69f15bf77
xerxex-odame <kingodame@yahoo.com>
2022-05-13 10:06:24 +0000

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name : "Lordia",
+    streetAddress : "0222 9525 twin palace road"
+  };
+  
+  
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObject = {...employee};
+    newObject[key]=value;
+    return newObject;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const newObject = employee;
+    newObject[key]=value;
+    return newObject;
+  }
+  
+  function deleteFromEmployeeByKey(employee,key);{
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+  ebb-ueni-uut};
